78c76d291c82fe4972bfd14c44c478942ba0c840
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-10-20 20:35:45 +0300

    lab 08 done

---
+++ app/src/test/java/quotes/AppTest.java
@@ -0,0 +1,26 @@
+package quotes;
+
+import com.google.gson.Gson;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testQuotes() {
+        Gson gson = new Gson();
+        try {
+            Reader reader = new FileReader("../app/src/main/resources/data.json");
+            Quotes[] recentQuotes = gson.fromJson(reader, Quotes[].class);
+            String expected = "Today's Quote: Quote:  “Ask no questions, and you'll be told no lies.�? author: Charles Dickens";
+            System.out.println(recentQuotes[1].toString());
+            Quotes output = recentQuotes[1];
+            assertEquals(expected,output);
+        } catch (IOException ex) {
+            System.out.println("file not found ... " + ex.getMessage());
+            assertTrue(false);
+        }
+    }
+    }
